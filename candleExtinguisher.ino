9635af5faa1e5ea25cb657811c1024f8f7c72a21
Janne Kerola <44394305+jkerola@users.noreply.github.com>
2019-04-17 13:26:14 +0300

    completed device logic

--- candleExtinguisher.ino
+++ candleExtinguisher.ino
@@ -1,9 +1,23 @@
 #include <Servo.h>
+//Pins
 Servo servo;
 int ledPin = 10;
 int servoPin = 8;
 int photoPin = A0;
-String n = "";
+
+//needed variables
+int lightSamples[10];
+int lightAvg;
+int lightLimit;
+
+// Key variables!
+int waitTime = 8;
+int lightThreshold = 15;
+
+int loopCounter = 0;
+int inactiveCounter = 0;
+boolean deviceActive = false;
+boolean calibrationRun = true;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,17 +29,79 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  Serial.println("pois");
-  Serial.println(analogRead(photoPin));
-  servo.write(180);
-  delay(2000);
   
+  // put your main code here, to run repeatedly:
+  //take 10 samples of candlelight
+  for(int i=0; i<10; i++){
+    delay(100);
+    lightSamples[i] = analogRead(photoPin);
+    //Serial.println(lightSamples[i]);
+  }
+  
+  //calculate average lightlevel
+  lightAvg = 0;
+  for(int i=0; i<10; i++){
+    lightAvg += lightSamples[i];
+  }
 
-  digitalWrite(ledPin, LOW);
-  Serial.println("pois");
-  Serial.println(analogRead(photoPin));
-  servo.write(0);
-  delay(2000);
+  lightAvg = lightAvg / 10;
+
+  //set default light levels as limit
+  if(calibrationRun){
+    lightLimit = lightAvg + lightThreshold;
+    calibrationRun = false;
+  }
+
+  Serial.println(lightAvg);
+
+  //turn on device if light level high enough
+  if(lightAvg >= lightLimit){
+    deviceActive = true;
+  } else{
+    deviceActive = false;
+  }
+
+  //main device logic
+  if(deviceActive){
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Kynttilä tunnistettu.");
+    loopCounter += 1;
+
+    //if counter reaches X seconds
+    if(loopCounter == waitTime){
+
+      //flash led
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+      digitalWrite(ledPin, LOW);
+      delay(100);
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+      digitalWrite(ledPin, LOW);
+      delay(100);
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+
+      //run servo
+      servo.write(180);
+      delay(1500);
+      servo.write(0);
+      digitalWrite(ledPin, LOW);
+      loopCounter = 0;
+      
+      }
+    } else{
+      
+      //wait a bit before resetting counters
+      delay(200);
+      inactiveCounter += 1;
+      Serial.println("Kynttilä sammunut.");
+      digitalWrite(ledPin, LOW);
+      if(inactiveCounter == 3){
+        loopCounter = 0;
+        inactiveCounter = 0;
+        Serial.println("Ajastin nollattu.");
+      }
+  }
+  
 }
